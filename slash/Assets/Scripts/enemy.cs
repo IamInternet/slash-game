b817d7d0377e5c61e692813a15d4a74239df4245
IamInternet <elliot.300338@nv.ccsd.net>
2023-04-04 09:51:48 -0700

    enemy air interaction

--- slash/Assets/Scripts/enemy.cs
+++ slash/Assets/Scripts/enemy.cs
@@ -9,12 +9,15 @@ public class enemy : MonoBehaviour
 	public Rigidbody rb;
 	public Transform et;
 	public Transform pt;
-
+	public LayerMask layerMask;
+	public bool isGrounded;
 	int hp = 2000;
 	double stun = 0; // if > 0 will not run enemy behaviour
 
 	void Update() {
 		rb.AddForce(Vector3.down * 10000 * Time.deltaTime);
+		isGrounded = Physics.CheckSphere(et.position, 2.5f, layerMask);
+		Debug.Log(isGrounded);
 	}
 
 	void OnTriggerEnter (Collider cInfo) {
@@ -29,31 +32,54 @@ public class enemy : MonoBehaviour
 				Destroy(gameObject);
 			}
 			if (player.lastAttack == player.SW5A) {
-				rb.AddForce(forward * 20000 * Time.deltaTime);
+				rb.AddForce(forward * 10000 * Time.deltaTime);
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 350000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW5AA) {
-				rb.AddForce(forward * 20000 * Time.deltaTime);
+				rb.AddForce(forward * 10000 * Time.deltaTime);
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW5A2A) {
-				rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				if (isGrounded) {
+					rb.AddForce(Vector3.up * 250000 * Time.deltaTime);
+				} else {
+				rb.AddForce(Vector3.up * 400000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW5AAA) {
-				rb.AddForce(forward * 200000 * Time.deltaTime);
+				rb.AddForce(forward * 500000 * Time.deltaTime);
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW5AA8A) {
-				rb.AddForce(forward * 200000 * Time.deltaTime);
+				rb.AddForce(forward * 600000 * Time.deltaTime);
 			}
 			if (player.lastAttack == player.SW2A) {
 				rb.AddForce(Vector3.up * 500000 * Time.deltaTime);
 			}
 			if (player.lastAttack == player.SW6A) {
 				rb.AddForce(forward * -1 * 20000 * Time.deltaTime);
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 350000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW6AA) {
-				rb.AddForce(forward * -1 * 200000 * Time.deltaTime);
+				rb.AddForce(forward * -1 * 500000 * Time.deltaTime);
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW8A) {
-				rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				if (isGrounded) {
+					rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				} else {
+				rb.AddForce(Vector3.up * 400000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW4A) {
 				if (pt.position.z < et.position.z) {
@@ -66,6 +92,9 @@ public class enemy : MonoBehaviour
 				} else {
 					rb.AddForce(right * 20000 * Time.deltaTime);
                 }
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 100000 * Time.deltaTime);
+				}
 			}
 			if (player.lastAttack == player.SW4AA) {
 				if (pt.position.z < et.position.z) {
@@ -78,6 +107,9 @@ public class enemy : MonoBehaviour
 				} else {
 					rb.AddForce(right * -1 * 500000 * Time.deltaTime);
                 }
+				if (!isGrounded) {
+					rb.AddForce(Vector3.up * 300000 * Time.deltaTime);
+				}
 			}
 
 		}
