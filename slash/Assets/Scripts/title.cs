120ecfbc061c3f9893ea6d56e86c6585a399f7b4
NKT-FOR-CODE <116135390+NKT-FOR-CODE@users.noreply.github.com>
2023-05-15 13:24:55 -0700

    fixed title and forestlevel

--- slash/Assets/Scripts/title.cs
+++ slash/Assets/Scripts/title.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +19,11 @@ public class title : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.H)) {
             t.text = "Use WASD to move.\nUse K to attack, and J or L to move the camera\nAttacking while holding a direction gives unique attacks\nSee movelist.txt for a full list of attacks\nDestroy all spawners then find the gate to win\nPress ENTER to begin";
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Application.Quit();
+            Debug.Log("QuitApplication");
         }
     }
 }
